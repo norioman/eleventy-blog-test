063e2e979b3829691107b7948b8688279eddd580
norioman <norio.star.man@gmail.com>
2025-04-18 12:53:50 +0900

    レイアウト適用と内容更新

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,16 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
-	return {
-		dir: {
-			input: "src",
-			output: "docs"
-		}
-	};
+  // date フィルター追加
+  eleventyConfig.addFilter("date", (value, format = "yyyy") => {
+    return DateTime.fromJSDate(new Date(value)).toFormat(format);
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "docs"
+    }
+  };
 };
+
--- src/index.md
+++ src/index.md
@@ -1,1 +1,9 @@
-# Hello, Eleventy!
+---
+title: Hello Eleventy
+layout: base.njk
+---
+
+# これはMarkdownから作られたページ
+
+テンプレートで自動的にHTMLになります。
+
